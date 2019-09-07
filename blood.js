649e84bc8ed62b65dbc0282e30421deef57e8ea0
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-09-07 17:29:47 -0500

    Added 'assignBloodBagReceiver' and 'moveBagToLocation' methods

--- blood.js
+++ blood.js
@@ -69,6 +69,8 @@
         let bloodBagOriginID = args[0]
         let bloodBagLocation = args[1]
         let bloodBagStatus = 'UNASIGNED'
+        let bloodBagReceiver = 'UNASIGNED'
+        let bloodBagDestination = 'UNASIGNED'
         let bloodType = args[2]
         let bloodRH = args[3]
         let bloodBagSize = parseInt(args[4])
@@ -90,6 +92,8 @@
         bloodBag.originId = bloodBagOriginID
         bloodBag.location = bloodBagLocation
         bloodBag.status = bloodBagStatus
+        bloodBag.recipient = bloodBagReceiver
+        bloodBag.destination = bloodBagDestination
         bloodBag.type = bloodType
         bloodBag.rh = bloodRH 
         bloodBag.size = bloodBagSize
@@ -176,4 +180,89 @@
         // Delete index entry to state
         await stub.deleteState(typeIdIndexKey)
      }
+
+     /**
+      * Mutate's a Blood Bag when moving to a new location
+      * @param {Object} stub Chaincode code executor
+      * @param {Object} args Bag Arguments such as bloodId and location
+      * @param {Object} thisClass References to this class
+      * @returns {Object} Function Execution Success Flag
+      */
+     async moveBagToLocation(stub,args,thisClass) {
+         // Input Sanitation
+        if (args.length < 2){
+            throw new Error('Incorrect number of arguments. Expecting Blood Bag\'s ID and new Location')
+        }
+        let ID = args[0]
+        let location = args[1]
+        console.info(' --- start moveBag',ID,'toLocation',location,'---')
+        //Query for a bag's information
+        let bagAsBytes = await stub.getState(ID)
+        if (!bagAsBytes || !bagAsBytes.toString()){
+            throw new Error('Bag ['+ID+'] does not exist')
+        }
+        
+        //Create a JSON for the bag
+        let bagToMove = {}
+        try {
+            bagToMove = JSON.parse(bagAsBytes.toString())
+        } catch (err) {
+            let jsonResp = {}
+            jsonResp.error = 'Failed to decode JSON of: '+ID
+            throw new Error(jsonResp)
+        }
+
+        //Change it's data
+        bagToMove.location = location
+        bagToMove.status = 'INTRANSIT'
+
+        //Rewrite it to the ledger
+        let bagJSONasBytes = Buffer.from(JSON.stringify(bagToMove))
+        await stub.putState(ID,bagJSONasBytes)
+        console.info(' --- end moveBagToLocation --- ')
+     }
+
+     /**
+      * Mutate's a Blood Bag when a recipient has been selected
+      * @param {Object} stub Chaincode code executor
+      * @param {Object} args Bag Arguments such as bloodId, recipient and Destination
+      * @param {Object} thisClass References to this class
+      * @returns {Object} Function Execution Success Flag
+      */
+     async assignBloodBagReceiver(stub,args,thisClass) {
+        // Input Sanitation
+       if (args.length < 3){
+           throw new Error('Incorrect number of arguments. Expecting Blood Bag\'s ID, recipientID and Destination')
+       }
+       let ID = args[0]
+       let recipient = args[1]
+       let destination = args[2]
+
+       console.info(' --- start assignBloodBagReceiver',ID,'---')
+       //Query for a bag's information
+       let bagAsBytes = await stub.getState(ID)
+       if (!bagAsBytes || !bagAsBytes.toString()){
+           throw new Error('Bag ['+ID+'] does not exist')
+       }
+       
+       //Create a JSON for the bag
+       let bagToMove = {}
+       try {
+           bagToMove = JSON.parse(bagAsBytes.toString())
+       } catch (err) {
+           let jsonResp = {}
+           jsonResp.error = 'Failed to decode JSON of: '+ID
+           throw new Error(jsonResp)
+       }
+
+       //Change it's data
+       bagToMove.recipient = recipient
+       bagToMove.destination = destination
+       bagToMove.status = 'ASSIGNED'
+
+       //Rewrite it to the ledger
+       let bagJSONasBytes = Buffer.from(JSON.stringify(bagToMove))
+       await stub.putState(ID,bagJSONasBytes)
+       console.info(' --- end assignBloodBagReceiver --- ')
+    }
  }
